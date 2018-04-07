df0f03d188cd53fd03ac1ffa89d760df79bf4973
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-07 15:52:36 +0700

    Add Hose Water Transition

--- Assets/Scripts/Fireman/FiremanController.cs
+++ Assets/Scripts/Fireman/FiremanController.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class FiremanController : MonoBehaviour {
 	public GameObject water;
-	public Vector3 OnClickedSize;
-	public Vector3 OnReleaseSize;
+	public float transitionTime;
+	public Vector3 onClickedSize;
+	public Vector3 onReleaseSize;
+	public Vector3 newSize;
+	private bool waterActive;
 	// Use this for initialization
 	void Start () {
 		water.SetActive(false);
 		CursorController.OnMouseDown += OnCursorDown;
 		CursorController.OnMouseUp += OnCursorUp;
+		newSize = onReleaseSize;
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+		ChangeSize();
+	}
+	public void ChangeSize() {
+		transform.localScale = Vector3.MoveTowards(transform.localScale,newSize,Time.deltaTime * transitionTime);
+	}
+	bool IsTransitionFinished() {
+		return (transform.localScale.x == newSize.x);
+	}
+	IEnumerator Transition() {
+		yield return new WaitUntil(() => IsTransitionFinished());
+		water.SetActive(waterActive);
 	}
 	public void OnCursorDown() {
-		water.SetActive(true);
+		newSize = onClickedSize;
+		waterActive = true;
+		StartCoroutine(Transition());
 	}
 	public void OnCursorUp() {
-		water.SetActive(false);
+		newSize = onReleaseSize;
+		waterActive = false;
+		water.SetActive(waterActive);
 	}
 }
