a3a08595c28177c723f6a2296960dda8b04c97c5
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-07 22:02:09 +0700

    Add Faces Animation

---
+++ Assets/Scripts/Doctor/Wound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wound : MonoBehaviour {
+	public Sprite[] woundSprites;
+	private int phaseScount;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
