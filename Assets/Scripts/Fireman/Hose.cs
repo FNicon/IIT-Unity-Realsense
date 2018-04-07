01fe4810e2701962603cd0bdb89b85a7e418e29e
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-07 15:00:05 +0700

    Add transparent water

--- Assets/Scripts/Fireman/Hose.cs
+++ Assets/Scripts/Fireman/Hose.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Water : MonoBehaviour {
+public class Hose : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
 		
