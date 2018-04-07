a526944a73711893b764c610840a5374fc7690df
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-07 15:31:43 +0700

    Add water active on click

--- Assets/Scripts/Fireman/FiremanController.cs
+++ Assets/Scripts/Fireman/FiremanController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FiremanController : MonoBehaviour {
-
+	public GameObject water;
+	public Vector3 OnClickedSize;
+	public Vector3 OnReleaseSize;
 	// Use this for initialization
 	void Start () {
-		
+		water.SetActive(false);
+		CursorController.OnMouseDown += OnCursorDown;
+		CursorController.OnMouseUp += OnCursorUp;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	public void OnCursorDown() {
+		water.SetActive(true);
+	}
+	public void OnCursorUp() {
+		water.SetActive(false);
 	}
 }
