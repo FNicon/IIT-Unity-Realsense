3a4bb2e2c18ac28c32271cc82be706c0d3939d8c
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-09 14:45:22 +0700

    Merge

---
+++ Assets/Scripts/Polisi/PolisiGameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolisiGameManager : MonoBehaviour {
+	public static PolisiGameManager instance = null;
+	// Use this for initialization
+	void Awake () {
+		if(instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+		InitGame();
+		TimeManager.Timesup += GameOver;
+	}
+	
+	void InitGame () {
+		PencuriSpawner.instance.SpawnRandom();
+	}
+
+	public void PencuriDead(){
+		PencuriSpawner.instance.SpawnRandomExceptLast();
+	}
+
+	public void GameOver(){
+		Destroy(GameObject.FindGameObjectWithTag("pencuri"));
+		Time.timeScale = 0;
+		Debug.Log("GAME OVER");
+		Debug.Log("Star = " + ScoreManager.instance.GetNumberOfStar());
+	}
+}
