cc3c8795d308c2c484fbbe6a4da874418a204902
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-23 00:43:22 +0700

    Add photo UI

--- Assets/Scripts/Characters/CharacterSelect.cs
+++ Assets/Scripts/Characters/CharacterSelect.cs
@@ -7,10 +7,9 @@ public class CharacterSelect : MonoBehaviour {
 	public RectTransform selectedObject;
 	public RectTransform[] childObjects;
 	// Use this for initialization
-	void Start () {
+	private void Awake() {
 		UpdateChildObjects();
-	}
-	
+	} 
 	// Update is called once per frame
 	void Update () {
 		
@@ -22,7 +21,13 @@ public class CharacterSelect : MonoBehaviour {
 			childObjects[i].SetSiblingIndex(i + 1);
 		}
 		childObjects[childObjects.Length - 1].SetSiblingIndex(0);
+		//ResetChildObject(3);
 		UpdateChildObjects();
+	}
+	void ResetChildObject(int previouslyChoosenChild) {
+		Vector3 currentChildScale = childObjects[previouslyChoosenChild].transform.localScale;
+		childObjects[previouslyChoosenChild].transform.localScale = new Vector3(
+			currentChildScale.x - 0.2f,currentChildScale.y - 0.2f,currentChildScale.z - 0.2f);
 	}
 	void UpdateChildObjects() {
 		int childCount = transform.childCount;
@@ -38,9 +43,12 @@ public class CharacterSelect : MonoBehaviour {
 			selectedObject.transform.localScale = Vector3.one;
 		}
 		selectedObject = childObjects[2];
+		//Vector3 currentScale = selectedObject.transform.localScale;
 		selectedObject.GetChild(0).gameObject.SetActive(true);
 		selectedObject.GetChild(0).transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
 		selectedObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+		/*selectedObject.transform.localScale = new Vector3(
+			currentScale.x + 0.2f, currentScale.y + 0.2f, currentScale.z + 0.2f);*/
 		selectedObject.GetComponent<Image>().color = Color.white;
 	}
 	public void SwipeLeft() {
@@ -50,6 +58,7 @@ public class CharacterSelect : MonoBehaviour {
 			childObjects[i].SetSiblingIndex(i - 1);
 		}
 		childObjects[0].SetSiblingIndex(childObjects.Length - 1);
+		//ResetChildObject(1);
 		UpdateChildObjects();
 	}
 }
