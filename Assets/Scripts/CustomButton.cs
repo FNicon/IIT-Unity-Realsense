e1edef2c6912b9b2c3957dedc88de0094db08b20
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-23 01:10:28 +0700

    Add Custom Button

---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomButton : MonoBehaviour {
+	public string mouseTag;
+	public SceneLoader sceneLoader;
+	public string sceneToLoad;
+	public bool isHover;
+	public Animator buttonAnimation;
+	// Use this for initialization
+	void Start () {
+		CursorController.OnMouseDown += OnCursorDown;
+		CursorController.OnMouseUp += OnCursorUp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerStay2D(Collider2D other){
+		if (other.CompareTag(mouseTag)) {
+			isHover = true;
+			//Debug.Log("Hover");
+		}
+	}
+	public void OnCursorDown(){
+		
+	}
+
+	public void OnCursorUp(){
+		if (isHover) {
+			sceneLoader.loadSpecificScene(sceneToLoad);	
+		}
+	}
+}
