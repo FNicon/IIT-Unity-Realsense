23972352567dba375b8d7afde9919e74d3a59a4a
Winarto <13515061@std.stei.itb.ac.id>
2018-05-16 23:34:56 +0700

    Add menu siapa aku & integrasi ui reward

--- Assets/Scripts/ClockRotator.cs
+++ Assets/Scripts/ClockRotator.cs
@@ -13,15 +13,23 @@ public class ClockRotator : MonoBehaviour {
 
 	private float startRotation_Minute;
 	private float startRotation_Hour;
-
+	bool gameOver = false;
 	void Start () {
 		startRotation_Hour = hourHand.rotation.eulerAngles.z;
 		startRotation_Minute = hourHand.rotation.eulerAngles.z;
+		TimeManager.Timesup += GameOver;
 	}
 	
 	void Update () {
+		if(gameOver)
+			return;
+			
 		float timeNorm = TimeManager.instance.GetTimeRemainingNormalized();
 		minuteHand.rotation = Quaternion.Euler(0,0,-timeNorm*numberOfRotation*360);
 		hourHand.rotation = Quaternion.Euler(0,0,startRotation_Hour+(-timeNorm*numberOfRotation*30));
+	}
+
+	public void GameOver (){
+		gameOver = true;
 	}
 }
--- Assets/Scripts/Fireman/FireGameManager.cs
+++ Assets/Scripts/Fireman/FireGameManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireGameManager : MonoBehaviour {
+	public GameObject endPanel;
 	void Awake () {
 		TimeManager.Timesup += GameOver;
 	}
 
 	public void GameOver(){
+		endPanel.SetActive(true);
+		Animator anim = endPanel.GetComponentInChildren<Animator>();
+		anim.SetInteger("STATE", ScoreManager.instance.GetNumberOfStar());
 		Time.timeScale = 0;
 		Debug.Log("GAME OVER");
 		Debug.Log("Star = " + ScoreManager.instance.GetNumberOfStar());
