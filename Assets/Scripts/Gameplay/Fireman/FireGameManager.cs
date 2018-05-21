004a28d8fba23f2c5f5f2aeaa51772e7067f8ce3
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-05-21 15:20:52 +0700

    Add transition

--- Assets/Scripts/Gameplay/Fireman/FireGameManager.cs
+++ Assets/Scripts/Gameplay/Fireman/FireGameManager.cs
@@ -12,7 +12,7 @@ public class FireGameManager : MonoBehaviour {
 		endPanel.SetActive(true);
 		Animator anim = endPanel.GetComponentInChildren<Animator>();
 		anim.SetInteger("STATE", ScoreManager.instance.GetNumberOfStar());
-		Time.timeScale = 0;
+		//Time.timeScale = 0;
 		Debug.Log("GAME OVER");
 		Debug.Log("Star = " + ScoreManager.instance.GetNumberOfStar());
 	}
--- Assets/Scripts/Scene/SceneLoader.cs
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 	public string sceneToLoad;
+	public Animator transition;
 	private string currentScene;
+	AsyncOperation operation;
 
 	// Use this for initialization
 	void Start () {
@@ -17,11 +19,27 @@ public class SceneLoader : MonoBehaviour {
 		
 	}
 
+	IEnumerator WaitLoadScene(string scene) {
+		yield return new WaitUntil(()=>transition.GetCurrentAnimatorStateInfo(0).IsName("Idle Full"));
+		StartCoroutine(AsyncLoad(scene));
+		operation.allowSceneActivation = true;
+	}
+
+	IEnumerator AsyncLoad(string scene) {
+		operation = SceneManager.LoadSceneAsync(scene);
+		operation.allowSceneActivation = false;
+		while (!operation.isDone) {
+			yield return null;
+		}
+	}
+
 	public void nextScene () {
 		SceneManager.LoadScene(sceneToLoad);
 	}
 	public void loadSpecificScene(string inputScene) {
-		SceneManager.LoadScene(inputScene);
+		transition.SetBool("isFadeIn",true);
+		StartCoroutine(WaitLoadScene(inputScene));
+		//SceneManager.LoadScene(inputScene);
 	}
 
 	public void ExitGame() {
