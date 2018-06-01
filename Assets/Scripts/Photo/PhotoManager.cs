83d8c70dcb29a3234d66a1b9426794707fbedf5d
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-06-01 21:10:04 +0700

    Cleaning debug log

--- Assets/Scripts/Photo/PhotoManager.cs
+++ Assets/Scripts/Photo/PhotoManager.cs
@@ -16,7 +16,7 @@ public class PhotoManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(photoVideo.frame);
+		//Debug.Log(photoVideo.frame);
 		if (IsVideoEnd()) {
 			PlayNext();
 		}
